29a6def2eb056688ee1d8263357ff85a74126964
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-04-04 21:56:48 -0400

    Playwright all, ini, find_by_name

--- W5D3/skeleton/plays.rb
+++ W5D3/skeleton/plays.rb
@@ -55,5 +55,26 @@ class Play
 
   def find_by_playwright(name)
     PlayDBConnection.instance.execute("SELECT * FROM playwright WHERE name = #{name}")
+    # comeback to this method
   end
+end
+
+class Playwright
+  attr_accessor :id, :name, :birth_year
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def find_by_name(name)
+    PlayDBConnection.instance.execute("SELECT * FROM playwright WHERE name = #{name}")
+  end
+
 end
